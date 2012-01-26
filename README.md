6f2133561952b1f7ec04e5faa1ef462117566e0c
Lucas Horton <lucas@lucashorton.com>
2012-01-26 14:13:19 -0600

    initial import

---
+++ README.md
@@ -0,0 +1,1 @@
+# Early Readiness High School Graduation Option System
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EarlyReadinessSystem::Application.config.secret_token = '596643aa87efdfa0712bfc560a17ebc16c6b6c8c5c372a95f1ecf8c13e4b6d2acb2ead069538d2ffd19573df60492bf1d0f27181285fd2776a6e9feb4e07dbed'
