5de467d1ee5f670324ebc7f284a4013c0977d918
Lucas Horton <lucas.horton@austin.utexas.edu>
2012-03-27 13:04:57 -0500

    removing unnecessary migrations

--- db/migrate/20120223000417_rename_scores_columns.rb
+++ db/migrate/20120223000417_rename_scores_columns.rb
@@ -1,8 +1,8 @@
 class RenameScoresColumns < ActiveRecord::Migration
   def change  	
-  	rename_column :scores, :subject_id, :student_id
-  	add_index "exams", ["subject_id"], :name => "index_exams_on_subject_id"
-  	add_index "scores", ["student_id"], :name => "index_scores_on_student_id"
-  	add_index "scores", ["exam_id"], :name => "index_scores_on_exam_id"
+  	#rename_column :scores, :subject_id, :student_id
+  	#add_index "exams", ["subject_id"], :name => "index_exams_on_subject_id"
+  	#add_index "scores", ["student_id"], :name => "index_scores_on_student_id"
+  	#add_index "scores", ["exam_id"], :name => "index_scores_on_exam_id"
   end
 end
--- db/migrate/20120228212914_remove_redundant_columns.rb
+++ db/migrate/20120228212914_remove_redundant_columns.rb
@@ -1,8 +1,8 @@
 class RemoveRedundantColumns < ActiveRecord::Migration
   def change
-  	remove_index :scores, :exam_id_id
-  	remove_index :scores, :student_id_id
-  	remove_index :exams, :subject_id_id
+  	#remove_index :scores, :exam_id_id
+  	#remove_index :scores, :student_id_id
+  	#remove_index :exams, :subject_id_id
   	remove_column :scores, :exam_id_id
   	remove_column :scores, :student_id_id
   	remove_column :exams, :subject_id_id
