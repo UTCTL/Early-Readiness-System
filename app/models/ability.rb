d9642686e3404c6a8d3460d28831787c0ef1ae45
Laura Davila <laura.davila@austin.utexas.edu>
2012-03-27 17:03:38 -0500

    add ability.rb

---
+++ app/models/ability.rb
@@ -0,0 +1,24 @@
+class Ability
+	include CanCan::Ability
+
+ def initialize(user)
+    
+    if AdminUser.find_by_eid(user) #user is an admin
+      
+      admin = AdminUser.find_by_eid(user)
+
+      if admin.role == 'SuperAdmin'
+      	can :manage, :all
+      elsif #not a super admin
+      	can :manage, Student 
+   	  end      
+    elsif Student.find_by_eid(user) #logged in as student
+      can :read, Student, :eid => user
+      can :create, Student
+    elsif
+    	can :create, Student
+    end
+  end
+
+
+end
