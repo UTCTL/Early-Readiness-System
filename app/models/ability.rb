e0692c069a89c6476a567b0fe8a47e00dff3949f
Laura Davila <laura.davila@austin.utexas.edu>
2012-06-11 14:58:15 -0500

    improve robusness of ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,12 +9,16 @@ class Ability
 
       if admin.role == 'SuperAdmin'
       	can :manage, :all
-      elsif #not a super admin
-      	can :manage, Student
-   	  end      
+      elsif admin.role == 'Highschool' #not a super admin
+      	can :manage, Student, :highschool_id => admin.highschool_id
+      elsif admin.role == 'District'
+        can :manage, Student, :highschool => {:district_id => admin.district_id}
+      elsif admin.role == 'University'
+        can :manage, Student, :universities => {:id => admin.university_id}
+        end      
     elsif Student.find_by_eid(user) #logged in as student
       can :read, Student, :eid => user
-      can :create, Student
+      can :create, Student, :eid => user
       can :manage, Student, :eid => user
     else
     	can :create, Student
